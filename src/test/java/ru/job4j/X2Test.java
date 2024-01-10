4f5210f47418acab85481807db471ebf681e56e9
Kife22 <Kife22@mail.ru>
2024-01-10 18:38:13 +0300

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/X2Test.java
+++ src/test/java/ru/job4j/X2Test.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
+import static ru.job4j.X2.calc;
 
 class X2Test {
     @Test
@@ -11,10 +12,9 @@ class X2Test {
         int c = 0;
         int x = 2;
         int expected = 40;
-        int result = X2.calc(a, b, c, x);
+        int result = calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
-
     @Test
     void whenA1B1C1X1Then3() {
         int a = 1;
@@ -22,10 +22,9 @@ class X2Test {
         int c = 1;
         int x = 1;
         int expected = 3;
-        int result = X2.calc(a, b, c, x);
+        int result = calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
-
     @Test
     void whenA0B1C1X1Then2() {
         int a = 0;
@@ -33,10 +32,9 @@ class X2Test {
         int c = 1;
         int x = 1;
         int expected = 2;
-        int result = X2.calc(a, b, c, x);
+        int result = calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
-
     @Test
     void whenA1B1C0X1Then2() {
         int a = 1;
@@ -44,19 +42,7 @@ class X2Test {
         int c = 0;
         int x = 1;
         int expected = 2;
-        int result = X2.calc(a, b, c, x);
-        assertThat(result).isEqualTo(expected);
-    }
-
-    @Test
-    void whenA1B1C1X0Then1() {
-        int a = 1;
-        int b = 1;
-        int c = 1;
-        int x = 0;
-        int expected = 1;
-        int result = X2.calc(a, b, c, x);
+        int result = calc(a, b, c, x);
         assertThat(result).isEqualTo(expected);
     }
 }
-
