fd6d37f22409781311838189e232ba69e1d31b62
Kife22 <Kife22@mail.ru>
2024-01-14 18:55:36 +0300

    4.6.1. Switch [#334070]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -34,7 +34,7 @@ public class SwitchWeek {
     }
 
     public static void main(String[] args) {
-        for (int j = 1; j < 8 ; j++) {
+        for (int j = 1; j < 8; j++) {
             System.out.println(nameOfDay(j));
         }
     }
