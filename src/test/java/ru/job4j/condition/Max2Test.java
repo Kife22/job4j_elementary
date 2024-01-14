23a70d12a7e408a0e90058fdf295cb4a26487980
Kife22 <Kife22@mail.ru>
2024-01-14 12:08:34 +0300

    4.2. Тернарное сравнение [#189]

--- src/test/java/ru/job4j/condition/Max2Test.java
+++ src/test/java/ru/job4j/condition/Max2Test.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class Max2Test {
     @Test
-    void whenMax1To2Then2() {
+    void whenMax1To2Then3() {
         int left = 1;
         int right = 2;
         int result = Max.max(left, right);
