3448101b5ef657fb7d06bf988349f944de6b5aa2
Kife22 <Kife22@mail.ru>
2024-01-16 18:08:50 +0300

    2

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -1,9 +1,11 @@
 package ru.job4j.condition;
 
 public class RectangleArea {
-    public static double square(double p, double k) {
-        double result = p / (2 * (k + 1)) * (p / (2 * (k + 1)) * k);
-        return result;
+    public static double square(int p, double k) {
+        double h = p / (2 * (k + 1));
+        double l = h * k;
+        double s = l * h;
+        return s;
     }
 
     public static void main(String[] args) {
--- src/test/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = number > 2;
-         for (int i = 2; i <=  Math.sqrt(number); i++) {
+         for (int i = 2; i <= number-1; i++) {
             if (number % i == 0) {
                 return false;
             }
